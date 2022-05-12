f02f6868970ce650c9e28213cb3db4a143c2a9f5
black-dragon <drhtunaungkyaw1993@gmail.com>
2022-05-13 00:24:12 +0630

    edit_sh

--- gitPush.sh
+++ gitPush.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 read -p 'Write your commit?: ' commit
-cd /home/Dr.404/Desktop/myClass/webhacking
-git add * & git commit -m $commit & git push -u origin main
+
+git add *  
+git commit -m $commit  
+git push -u origin main
